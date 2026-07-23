8e7bbab2957880a5325f967d4ff89ed2917aa566
Stanislav Stoyanov <grimerssy@gmail.com>
2026-07-23 23:56:51 +0200

    Fix GOPATH in home-manager to be an absolute path (#71)

--- modules/home/configurations/gopath.nix
+++ modules/home/configurations/gopath.nix
@@ -1,8 +1,8 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   programs.go = {
     enable = true;
     package = pkgs.emptyDirectory;
-    env.GOPATH = ".go";
+    env.GOPATH = "${config.home.homeDirectory}/.go";
   };
 }
